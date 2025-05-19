c9ccf270f2d7673694311577b3203adc3f9677e8
“MDDO0” <maado321@gmail.com>
2025-05-19 21:06:46 +0000

    “a”

--- python-test_driven_development/100-matrix_mul.py
+++ python-test_driven_development/100-matrix_mul.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
-"""
-This module defines a function for matrix multiplication.
-"""
+"""Module for matrix multiplication."""
 
 
 def matrix_mul(m_a, m_b):
-    """Multiplies two matrices."""
+    """Multiplies two matrices m_a and m_b.
+
+    Args:
+        m_a: list of lists of ints/floats
+        m_b: list of lists of ints/floats
+
+    Returns:
+        A new matrix that is the product of m_a and m_b.
+
+    Raises:
+        TypeError, ValueError: if inputs are invalid
+    """
+
     if not isinstance(m_a, list):
         raise TypeError("m_a must be a list")
     if not isinstance(m_b, list):
@@ -21,26 +31,33 @@ def matrix_mul(m_a, m_b):
     if m_b == [] or m_b == [[]]:
         raise ValueError("m_b can't be empty")
 
-    if not all(isinstance(ele, (int, float)) for row in m_a for ele in row):
+    if not all(
+        isinstance(el, (int, float)) for row in m_a for el in row
+    ):
         raise TypeError("m_a should contain only integers or floats")
-    if not all(isinstance(ele, (int, float)) for row in m_b for ele in row):
+    if not all(
+        isinstance(el, (int, float)) for row in m_b for el in row
+    ):
         raise TypeError("m_b should contain only integers or floats")
 
-    if not all(len(row) == len(m_a[0]) for row in m_a):
+    row_length = len(m_a[0])
+    if not all(len(row) == row_length for row in m_a):
         raise TypeError("each row of m_a must be of the same size")
-    if not all(len(row) == len(m_b[0]) for row in m_b):
+    row_length = len(m_b[0])
+    if not all(len(row) == row_length for row in m_b):
         raise TypeError("each row of m_b must be of the same size")
 
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
 
     result = []
-    for row in m_a:
+    for i in range(len(m_a)):
         new_row = []
-        for i in range(len(m_b[0])):
+        for j in range(len(m_b[0])):
             total = 0
-            for j in range(len(m_b)):
-                total += row[j] * m_b[j][i]
+            for k in range(len(m_b)):
+                total += m_a[i][k] * m_b[k][j]
             new_row.append(total)
         result.append(new_row)
+
     return result
