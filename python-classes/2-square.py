10057a623be5064696fbb37271679d1759442326
“MDDO0” <maado321@gmail.com>
2025-05-22 10:02:20 +0000

    “a”

---
+++ python-classes/2-square.py
@@ -0,0 +1,14 @@
+class Square:
+    """Defines a square by its size"""
+
+    def __init__(self, size=0):
+        """Initialize square with size
+
+        Args:
+            size (int): The size of a side of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
