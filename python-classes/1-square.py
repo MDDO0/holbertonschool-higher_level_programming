26aee47870b5d733d3529aac323064de394a7c17
“MDDO0” <maado321@gmail.com>
2025-05-22 09:42:30 +0000

    “a”

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""Defines a class Square."""
+
+
 class Square:
+    """Represents a square."""
     def __init__(self, size):
+        """Initializes a new Square.
+
+        Args:
+            size: The size of the square.
+        """
         self.__size = size
