ed046f8c1c6e4768dd3656e5fb87e3384518d4e9
“MDDO0” <maado321@gmail.com>
2025-06-13 20:35:06 +0000

    “code”

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-"""This module provides a function that returns the list of available attributes and methods of an object."""
+"""
+This module provides a function that returns the list of
+available attributes and methods of an object.
+"""
+
 
 def lookup(obj):
     """Returns a list of available attributes and methods of an object."""
