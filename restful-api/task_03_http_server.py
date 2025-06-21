3bf809d64f66cee01b456f2c3106345415015f78
“MDDO0” <maado321@gmail.com>
2025-06-21 19:23:04 +0000

    “code”

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -34,7 +34,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(b"404 Not Found")
+            self.wfile.write(b"Endpoint not found")
 
 
 if __name__ == "__main__":
