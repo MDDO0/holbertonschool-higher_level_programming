c33343117765e215bc967135626cb4bf76bcc23a
“MDDO0” <maado321@gmail.com>
2025-06-29 17:23:41 +0000

    code

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- Task 0: Show all privileges of user_0d_1 and user_0d_2
+-- This query displays privileges for both users on localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
